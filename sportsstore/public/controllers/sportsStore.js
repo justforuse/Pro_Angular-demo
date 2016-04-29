c7ba1d8412211cc33c9325dbda2ed4bdb3b0cbe8
justforuse <yuanxiaodong@pachiratech.com>
2016-04-30 00:36:56 +0800

    4.30

--- sportsstore/public/controllers/sportsStore.js
+++ sportsstore/public/controllers/sportsStore.js
@@ -1,14 +1,25 @@
 angular.module("sportsStore")
-	.controller("sportsStoreCtrl", function($scope){
-		$scope.data={
-			products:[
-			{ name: "Product #1", description: "A product",
-			category: "Category #1", price: 100 },
-			{ name: "Product #2", description: "A product",
-			category: "Category #1", price: 110 },
-			{ name: "Product #3", description: "A product",
-			category: "Category #2", price: 210 },
-			{ name: "Product #4", description: "A product",
-			category: "Category #3", price: 202 }]
-		}
+	.constant("dataUrl", "http://localhost:2403/products")
+	.controller("sportsStoreCtrl", function($scope, $http, dataUrl){
+
+		$scope.data={};
+		$http.get(dataUrl)
+			.success(function(data){
+				$scope.data.products=data;
+			})
+			.error(function(error){
+				$scope.data.error=error;
+			})
+
+		// $scope.data={
+		// 	products:[
+		// 	{ name: "Product #1", description: "A product",
+		// 	category: "Category #1", price: 100 },
+		// 	{ name: "Product #2", description: "A product",
+		// 	category: "Category #1", price: 110 },
+		// 	{ name: "Product #3", description: "A product",
+		// 	category: "Category #2", price: 210 },
+		// 	{ name: "Product #4", description: "A product",
+		// 	category: "Category #3", price: 202 }]
+		// }
 	})
